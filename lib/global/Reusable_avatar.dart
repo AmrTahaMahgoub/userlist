ac39ac9883aff276f1fde06d0176f324dd142da3
amr <chemistamrmahgoub@gmail.com>
2022-07-17 22:42:09 +0200

    new constants file

---
+++ lib/global/Reusable_avatar.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AvatarModel extends StatelessWidget {
+  const AvatarModel({required this.avatarcolor, required this.widget, required this.avatarRaduis});
+  final Color avatarcolor ;
+  final Widget widget;
+  final double avatarRaduis;
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(backgroundColor:avatarcolor ,
+    child: widget,
+    radius: avatarRaduis,);
+  }
+}
